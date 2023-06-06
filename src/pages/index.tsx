b2cfae1283569d6b63a25b68fde80d7a0e6b2527
danilocucharro <danicucharro@outlook.com>
2023-06-06 10:25:24 -0300

    estilização base da pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,29 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '../../styles/home.module.css'
+import Image from 'next/image'
+
+import heroImg from '../../public/assets/hero.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Tarefas+ | Organize suas tarefas de forma elegante</title>
       </Head>
-      <h1>Meu Projeto</h1>
-    </>
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="Logo Tarefas+"
+            src={heroImg}
+            priority
+          />
+          <h1 className={styles.title}>Sistema feito para você organizar <br/>melhor suas tarefas diárias.</h1>
+        </div>
+      </main>
+    </div>
   )
 }
