0580a53bad39c368809824cc2413157bab7fe961
danilocucharro <danicucharro@outlook.com>
2023-06-06 16:46:10 -0300

    criado a verificação do login para o acesso a pagina dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps } from 'next'
 import styles from "./styles.module.css"
 import Head from 'next/head'
+
+import { getSession } from 'next-auth/react'
 
 export default function Dashboard(){
   return(
@@ -12,3 +15,22 @@ export default function Dashboard(){
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req })=>{
+  const session = await getSession({ req })
+  //console.log(session);
+
+  if(!session?.user){
+    // se nao esta logado volta pra home
+    return{
+      redirect:{
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
+  return{
+    props: {},
+  };
+};
