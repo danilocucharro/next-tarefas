784a4d55f3f81c1f0299e74ff930927ec10eca85
danilocucharro <danicucharro@outlook.com>
2023-06-28 08:58:41 -0300

    pagina de detalhes e comentarios estilizada

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -10,17 +10,44 @@ import {
   query,
   where 
 } from 'firebase/firestore'
+import { Textarea } from '@/src/components/textarea';
 
-export default function task(){
+interface TaskProps{
+  item: {
+    tarefa: string;
+    createdAt: string;
+    public: boolean;
+    user: string;
+    taskId: string
+  }
+}
+
+export default function Task({ item }: TaskProps){
   return(
     <div className={styles.container}>
       <Head>
-        <title>Detalhes da Tarefa</title>
+        <title>Tarefa - Detalhes da tarefa</title>
       </Head>
 
       <main className={styles.main}>
         <h1>Tarefa</h1>
+        <article className={styles.task}>
+          <p>
+            {item.tarefa}
+          </p>
+        </article>
       </main>
+
+      <section className={styles.comentsContainer}>
+        <h2>Deixar comentário</h2>
+
+        <form>
+          <Textarea
+            placeholder="Digite seu comentário..."
+          />
+          <button className={styles.button}>Enviar Comentário</button>
+        </form>
+      </section>
     </div>
   )
 }
@@ -62,7 +89,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 
   return{
     props:{
-
+      item: task,
     }
   }
 }
